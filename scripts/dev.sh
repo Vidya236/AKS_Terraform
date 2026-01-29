bf2f5868f16e6e7d81a68645cfd1ae71cf95b5e6
Vidya Vallika Nimmana <125736138+Vidya236@users.noreply.github.com>
2026-01-29 16:02:24 +0530

    Update dev.sh

---
+++ scripts/dev.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+RESOURCE_GROUP_NAME=terraform-state-rg
+STAGE_SA_ACCOUNT=tfenv_stagebackend
+DEV_SA_ACCOUNT=tfenv_devbackend
+CONTAINER_NAME=tfstate
+
+
+# Create resource group
+az group create --name $RESOURCE_GROUP_NAME --location southindia
+
+# Create storage account for staging environment
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STAGE_SA_ACCOUNT --sku Standard_LRS --encryption-services blob
+
+# Create storage account for dev environment
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $DEV_SA_ACCOUNT --sku Standard_LRS --encryption-services blob
+
+# Create blob container for staging environment
+az storage container create --name $CONTAINER_NAME --account-name $STAGE_SA_ACCOUNT
+
+# Create blob container for dev environment
+az storage container create --name $CONTAINER_NAME --account-name $DEV_SA_ACCOUNT
